61f68f75b23428f43067ad81ef5ec35f4df5409e
Sergii Nevydanchuk <sergii.nevydanchuk@gmail.com>
2014-04-24 14:52:52 +0300

    Added backbone, main.js, underscore and other libraries, also added application.html placeholder for main app, other views were designed.

---
+++ JavascriptApp/js/main.js
@@ -0,0 +1,44 @@
+// models
+var LoginModel = Backbone.model.extend(
+    {
+
+    }
+);
+
+var RestorePasswordModel = Backbone.model.extend(
+    {
+
+    }
+);
+
+var GroupsCollection = Backbone.collection.extend(
+    {
+
+    }
+);
+
+var Group = Backbone.model.extend(
+    {
+
+    }
+);
+
+// views
+
+var LoginView = Backbone.view.extend({
+
+});
+
+var ForgotPasswordView = Backbone.view.extend(
+    {
+
+    }
+);
+
+var GroupsListView = Backbone.view.extend({
+
+});
+
+var GroupDetailsView = Backbone.view.extend({
+
+});
