cb0d4020e99c68b590e4268e7d25cba3eac9dd9c
Sergii Nevydanchuk <sergii.nevydanchuk@gmail.com>
2014-04-29 07:49:42 +0300

    Added beginning implementation for views and models.

--- JavascriptApp/js/main.js
+++ JavascriptApp/js/main.js
@@ -1,13 +1,31 @@
 // models
 var LoginModel = Backbone.model.extend(
     {
+        defaults: {
+            "email": "",
+            "password": ""
+        }
+    }
+);
 
+var SignUpModel = Backbone.model.extend(
+    {
+        defaults: {
+            "email": "",
+            "password": "",
+            "repeatPassword": "",
+            "firstName": "",
+            "lastName": "",
+            "phone": ""
+        }
     }
 );
 
 var RestorePasswordModel = Backbone.model.extend(
     {
-
+        defaults: {
+            "email": ""
+        }
     }
 );
 
@@ -19,7 +37,10 @@ var GroupsCollection = Backbone.collection.extend(
 
 var Group = Backbone.model.extend(
     {
-
+        defaults: {
+            "name": "",
+            "dateCreated": ""
+        }
     }
 );
 
@@ -27,18 +48,84 @@ var Group = Backbone.model.extend(
 
 var LoginView = Backbone.view.extend({
 
+    tagName: "div",
+
+    events: {
+        "click #signIn": "signIn"
+    },
+
+    initialize: function () {
+        this.listenTo(this.model, "change", this.render);
+    },
+
+    signIn: function () {
+        console.log("Doing sign in");
+    }
+
+});
+
+var SignUpView = Backbone.view.extend({
+
+    tagName: "div",
+
+    events: {
+        "click #signUp": "signUp"
+    },
+
+    initialize: function () {
+        this.listenTo(this.model, "change", this.render);
+    },
+
+    signUp: function () {
+        console.log("Doing sign up operation");
+    }
+
 });
 
 var ForgotPasswordView = Backbone.view.extend(
     {
+
+        tagName: "div",
+
+        events: {
+            "click #forgotPassword": "restorePassword"
+        },
+
+        initialize: function () {
+            this.listenTo(this.model, "change", this.render);
+        },
+
+        restorePassword: function () {
+            console.log('Restoring password');
+        }
 
     }
 );
 
 var GroupsListView = Backbone.view.extend({
 
+    tagName: "div",
+
+    events: {
+
+    },
+
+    initialize: function () {
+        this.listenTo(this.model, "change", this.render);
+    }
+
 });
 
 var GroupDetailsView = Backbone.view.extend({
+
+    tagName: "div",
+
+    events: {
+
+    },
+
+    initialize: function () {
+        this.listenTo(this.model, "change", this.render);
+    }
 
 });
